7d2b0961e07efb6d824a8d6588f43920e1b29ab6
Finn Ehlers <finndows@outlook.de>
2024-05-11 13:23:18 +0200

    added test for recieving published topics

--- test/test_qr_code_detection.py
+++ test/test_qr_code_detection.py
@@ -28,7 +28,7 @@ def test_qrcode_info_publish():
         assert msg.error_code == 5
 
         payload_check = {}
-        payload_check["marker"] = "42"
+        payload_check["marker"] = "6"
         assert json.loads(msg.payload) == payload_check
 
         assert not qr_scanner_node.active
@@ -42,7 +42,7 @@ def test_qrcode_info_publish():
         test_node.get_logger().debug("Sending job_finished successfull message")
 
         payload = {}
-        payload["marker"] = "42"
+        payload["marker"] = "6"
         qr_scanner_node._job_finished_custom_(5, payload)
 
     job_finished_timer = qr_scanner_node.create_timer(
@@ -66,26 +66,96 @@ def test_activate_with_control_message():
     control_publisher = test_node.create_publisher(
         Control, TopicNames.Control, 10)
 
-    def timer_callback():
-        nonlocal test_node
-        test_node.get_logger().debug("Sending control message to activate QRCodeScannerNode")
-        print("Test print")
-        msg = Control()
-        msg.target_id = "qr_scanner_node"
-        msg.active = True
-        msg.payload = ""
+    test_node.get_logger().debug("Sending control message to activate QRCodeScannerNode")
+    print("Test print")
+    msg = Control()
+    msg.target_id = "qr_scanner_node"
+    msg.active = True
+    msg.payload = ""
 
-        control_publisher.publish(msg)
+    control_publisher.publish(msg)
 
     def end_timer_callback():
         assert qr_scanner_node.active
         executor.shutdown(0)
 
-    publish_control_timer = test_node.create_timer(
-        0.1, timer_callback)
-
     end_timer = test_node.create_timer(
-        0.2, end_timer_callback)
+        0.01, end_timer_callback)
+
+    executor.add_node(qr_scanner_node)
+    executor.add_node(test_node)
+
+    executor.spin()
+
+    del executor
+    
+    
+def test_activate_and_recieve_content_job_finished():
+    executor = SingleThreadedExecutor()
+
+    qr_scanner_node = QRCodeScannerNode("qr_scanner_node")
+    assert not qr_scanner_node.active
+
+    test_node = Node("test")
+
+    control_publisher = test_node.create_publisher(
+        Control, TopicNames.Control, 10)
+
+    msg = Control()
+    msg.target_id = "qr_scanner_node"
+    msg.active = True
+    msg.payload = ""
+
+    control_publisher.publish(msg)
+
+    def job_finished_callback(msg):
+        assert msg.sender_id == "qr_scanner_node"
+        assert msg.error_code == 0
+
+        payload_check = {}
+        payload_check["marker"] = "42"
+        assert json.loads(msg.payload) == payload_check
+        assert not qr_scanner_node.active
+        executor.shutdown(0)
+
+    job_finished_sub = test_node.create_subscription(
+        JobFinished, TopicNames.JobFinished, job_finished_callback, 10)
+
+    executor.add_node(qr_scanner_node)
+    executor.add_node(test_node)
+
+    executor.spin()
+
+    del executor
+    
+    
+def test_activate_and_recieve_content_QRCode_Info():
+    executor = SingleThreadedExecutor()
+
+    qr_scanner_node = QRCodeScannerNode("qr_scanner_node")
+    assert not qr_scanner_node.active
+
+    test_node = Node("test")
+
+    control_publisher = test_node.create_publisher(
+        Control, TopicNames.Control, 10)
+
+    msg = Control()
+    msg.target_id = "qr_scanner_node"
+    msg.active = True
+    msg.payload = ""
+
+    control_publisher.publish(msg)
+
+    def qrcode_info_callback(msg):
+        assert msg.sender_id == "qr_scanner_node"
+        assert msg.qr_code_content is not None
+        assert msg.qrcode_position_x is not None
+        assert msg.qrcode_position_y is not None
+        executor.shutdown(0)
+
+    qr_code_info_sub = test_node.create_subscription(
+        QRCodeInfo, TopicNames.QRCodeInfo, qrcode_info_callback, 10)
 
     executor.add_node(qr_scanner_node)
     executor.add_node(test_node)
